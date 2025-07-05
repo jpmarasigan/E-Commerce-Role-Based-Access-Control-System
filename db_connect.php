55a58aec5277e94b1e440da70d9ae954e1cdde06
John Patrick Marasigan <patrickmarasigan06@gmail.com>
2025-07-05 19:48:11 +0800

    [ADD] local first

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,14 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = 'marasigan';
-$db = 'ecommerce';
+echo "PHP is working";
+// $host = 'localhost';
+// $user = 'root';
+// $pass = 'marasigan';
+// $db = 'ecommerce';
 
 
-// Create database connection
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) {
-    die("Connection failed: ".$conn->connect_error);
-}
+// // Create database connection
+// $conn = new mysqli($host, $user, $pass, $db);
+// if ($conn->connect_error) {
+//     die("Connection failed: ".$conn->connect_error);
+// }
 ?>
